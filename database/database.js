cfa23e8b1d0b505587a99ea1002f2ae9cbb1dd55
RainbowFloyd <rainbowfloyd911@gmail.com>
2018-09-04 03:17:25 -0700

    Connected to MongoDB

---
+++ database/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/bibleVerseStudy');
+
+let db = mongoose.connection;
+
+db.on('error', function() {
+	console.log('mongoose connection error');
+});
+
+db.once('open', function() {
+	console.log('mongoose connected successfully')
+})
+
+module.exports = db
